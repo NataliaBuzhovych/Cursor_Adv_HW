b9d4e2a1dc5ae595bed380a56dface60d02c3bd7
Natalia Buzhovych <tuzhay@gmail.com>
2022-09-09 14:40:27 +0300

    HomeWork-07

---
+++ HomeWork-07/main.js
@@ -0,0 +1,34 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+// function getMyTaxes(salary) {
+//   return salary - this.tax * salary;
+// }
+// console.log(getMyTaxes.call(ukraine, 1000));
+
+// function getMiddleTaxes() {
+//   return this.tax * this.middleSalary;
+// }
+
+// console.log(getMiddleTaxes.call(ukraine));
+
+// function getTotalTaxes() {
+//   return this.tax * this.middleSalary * this.vacancies;
+// }
+// console.log(getTotalTaxes.call(litva));
+
+function getMySalary() {
+  const salary = Math.floor(Math.random() * 500) + 1500;
+  const taxes = Math.round(salary * this.tax);
+  const profit = salary - taxes;
+  let mySalary = {
+    salary,
+    taxes,
+    profit,
+  };
+  console.log(mySalary);
+}
+let interval = setInterval(() => getMySalary.call(ukraine), 10000);
