6cafc5487a01f29f094340eef3cacc4291dafbdd
Natalia Buzhovych <tuzhay@gmail.com>
2022-10-12 12:25:12 +0300

    HomeWork-04

--- HomeWork-04/main.js
+++ HomeWork-04/main.js
@@ -1,45 +1,53 @@
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
-const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const themes = [
+  "Диференційне рівняння",
+  "Теорія автоматів",
+  "Алгоритми і структури даних",
+];
 
 const marks = [4, 5, 5, 3, 4, 5];
 
 const girls = [];
 const boys = [];
 for (let i = 0; i < students.length; i++) {
-    if (students[i].lastIndexOf("а") === students[i].length-1) {
-        girls.push(students[i]);
-    } else {
-        boys.push(students[i]);
-    }
+  if (students[i].lastIndexOf("а") === students[i].length - 1) {
+    girls.push(students[i]);
+  } else {
+    boys.push(students[i]);
+  }
 }
 
 const studentsCouple = [];
 for (let i = 0; i < girls.length; i++) {
-    studentsCouple.push([boys[i], girls[i]]);
+  studentsCouple.push([boys[i], girls[i]]);
 }
 console.log(studentsCouple);
 
 const studentsAndTopics = [];
 
 for (let i = 0; i < themes.length; i++) {
-    studentsAndTopics.push([boys[i] + ' і ' + girls[i], themes[i]]);
+  studentsAndTopics.push([boys[i] + " і " + girls[i], themes[i]]);
 }
 console.log(studentsAndTopics);
 
 const studentsAndMarks = [];
 
-for (let i = 0; i< marks.length; i++) {
-    studentsAndMarks.push([students[i], marks[i]]);
+for (let i = 0; i < marks.length; i++) {
+  studentsAndMarks.push([students[i], marks[i]]);
 }
 console.log(studentsAndMarks);
 
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const studentsAndTopicsAndMarks = [];
 for (let i = 0; i < themes.length; i++) {
-    studentsAndTopicsAndMarks.push([boys[i] + ' і ' + girls[i], themes[i], getRandomNumber(1, 5)]);
+  studentsAndTopicsAndMarks.push([
+    boys[i] + " і " + girls[i],
+    themes[i],
+    getRandomNumber(1, 5),
+  ]);
 }
 console.log(studentsAndTopicsAndMarks);
